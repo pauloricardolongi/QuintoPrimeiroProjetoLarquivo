19102a6d0c3cf711c23b0801c6402c7729b7e096
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-21 03:24:25 -0300

    pegar somente o nome do arquivo

--- src/bb/Programa.java
+++ src/bb/Programa.java
@@ -8,23 +8,14 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Entre com folder path: ");
+		
+		System.out.println("Entre com file path: ");
 		String strPath = sc.nextLine();
-		
 		File path = new File(strPath);
+	//acessar  somente o nome do arquivo
 		
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDER:");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		File[] files = path.listFiles(File:: isFile);
-		System.out.println(" FILES");
-		for(File file : files) {
-			System.out.println(file);
-		}
-		boolean sucesso = new File(strPath + "\\subdividir").mkdir();
-		System.out.println("Diretório criado com sucesso: " + sucesso);
+		System.out.println("getNome: "+ path.getName());
+		
 		sc.close();
 		
 	}
